b16db73fca845f9682693b05ded46d02d24cc349
Freeman-md <freemanmadudili@gmail.com>
2021-07-12 20:25:36 +0100

    Refactored into React Components and Stateless Components

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,117 +1,241 @@
 'use strict';
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
 var appRoot = document.getElementById('app');
 
-var getTodos = function getTodos() {
-  return JSON.parse(localStorage.getItem('todos')) || [];
-};
+var TodoApp = function (_React$Component) {
+  _inherits(TodoApp, _React$Component);
 
-var addTodo = function addTodo(e) {
-  e.preventDefault();
-  var title = e.target.elements.title.value;
-  var description = e.target.elements.description.value;
+  function TodoApp(props) {
+    _classCallCheck(this, TodoApp);
 
-  if (title) {
-    var todos = getTodos();
-    var todo = {
-      id: uuid.v4(),
-      title: title,
-      description: description,
-      date: moment().valueOf()
+    var _this = _possibleConstructorReturn(this, (TodoApp.__proto__ || Object.getPrototypeOf(TodoApp)).call(this, props));
+
+    _this.removeTodo = _this.removeTodo.bind(_this);
+    _this.loadTodos = _this.loadTodos.bind(_this);
+    _this.saveTodos = _this.saveTodos.bind(_this);
+    _this.state = {
+      todos: []
     };
-    todos.push(todo);
-    localStorage.setItem('todos', JSON.stringify(todos));
-
-    e.target.elements.title.value = '';
-    e.target.elements.description.value = '';
-
-    render();
+    return _this;
   }
-};
 
-var renderTodos = function renderTodos() {
-  return getTodos().sort(function (a, b) {
-    return a.date >= b.date ? -1 : 1;
-  }).map(function (todo) {
-    return generateDOM(todo);
-  });
-};
+  _createClass(TodoApp, [{
+    key: 'componentDidMount',
+    value: function componentDidMount() {
+      this.loadTodos();
+    }
+  }, {
+    key: 'removeTodo',
+    value: function removeTodo(id) {
+      this.setState(function (prevState) {
+        return {
+          todos: prevState.todos.filter(function (todo) {
+            return todo.id != id;
+          })
+        };
+      });
+      this.saveTodos();
+    }
+  }, {
+    key: 'loadTodos',
+    value: function loadTodos() {
+      this.setState(function () {
+        return {
+          todos: JSON.parse(localStorage.getItem('todos')) || []
+        };
+      });
+    }
+  }, {
+    key: 'saveTodos',
+    value: function saveTodos(todo) {
+      var todos = this.state.todos;
+      if (todo) {
+        todos.push(todo);
+        this.setState(function () {
+          return { todos: todos };
+        });
+      }
 
-var generateDOM = function generateDOM(todo) {
-  return React.createElement(
-    'li',
-    { className: 'list-group-item d-flex justify-content-between align-items-start', id: todo.id, key: todo.id },
-    React.createElement(
-      'div',
-      { className: 'ms-2 me-auto' },
-      React.createElement(
+      localStorage.setItem('todos', JSON.stringify(this.state.todos));
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return React.createElement(
         'div',
-        { className: 'fw-bold' },
-        todo.title
-      ),
-      todo.description
-    ),
-    React.createElement(
-      'button',
-      { className: 'badge bg-primary rounded-pill', onClick: function onClick(e) {
-          return removeTodo(e, todo.id);
-        } },
-      '\xD7'
-    )
-  );
-};
+        { className: 'container' },
+        React.createElement(
+          'div',
+          { className: 'row' },
+          React.createElement(
+            'div',
+            { className: 'py-2 col-md-6 col-sm-8 col-12 offset-md-3 offset-sm-2' },
+            React.createElement(Header, null),
+            React.createElement(AddTodo, { saveTodo: this.saveTodos }),
+            React.createElement(Todos, { todos: this.state.todos, removeTodo: this.removeTodo })
+          )
+        )
+      );
+    }
+  }]);
 
-var removeTodo = function removeTodo(e, id) {
-  localStorage.setItem('todos', JSON.stringify(getTodos().filter(function (todo) {
-    return todo.id != id;
-  })));
-  render();
-};
+  return TodoApp;
+}(React.Component);
 
-var render = function render() {
+var Todos = function (_React$Component2) {
+  _inherits(Todos, _React$Component2);
 
-  var todos = getTodos();
+  function Todos(props) {
+    _classCallCheck(this, Todos);
 
-  var template = React.createElement(
-    'div',
-    { className: 'container' },
-    React.createElement(
-      'div',
-      { 'class': 'row' },
-      React.createElement(
+    var _this2 = _possibleConstructorReturn(this, (Todos.__proto__ || Object.getPrototypeOf(Todos)).call(this, props));
+
+    _this2.renderTodos = _this2.renderTodos.bind(_this2);
+    return _this2;
+  }
+
+  _createClass(Todos, [{
+    key: 'renderTodos',
+    value: function renderTodos() {
+      var _this3 = this;
+
+      return this.props.todos.sort(function (a, b) {
+        return a.date >= b.date ? -1 : 1;
+      }).map(function (todo) {
+        return React.createElement(Todo, { key: todo.id, todo: todo, removeTodo: _this3.props.removeTodo });
+      });
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return React.createElement(
         'div',
-        { 'class': 'col-md-6 col-sm-8 col-12 offset-md-3 offset-sm-2' },
-        React.createElement(
-          'h1',
-          null,
-          'Todo Application'
-        ),
-        React.createElement(
+        null,
+        this.props.todos.length > 0 && React.createElement(
           'h4',
           null,
           'Todos'
         ),
-        todos && React.createElement(
+        this.props.todos && React.createElement(
           'ol',
-          { className: 'list-group mb-2 list-group-numbered list-group-flush', id: 'todos' },
-          renderTodos()
+          { className: 'mb-2 list-group list-group-numbered list-group-flush', id: 'todos' },
+          this.renderTodos()
+        )
+      );
+    }
+  }]);
+
+  return Todos;
+}(React.Component);
+
+var Todo = function (_React$Component3) {
+  _inherits(Todo, _React$Component3);
+
+  function Todo(props) {
+    _classCallCheck(this, Todo);
+
+    return _possibleConstructorReturn(this, (Todo.__proto__ || Object.getPrototypeOf(Todo)).call(this, props));
+  }
+
+  _createClass(Todo, [{
+    key: 'render',
+    value: function render() {
+      var _this5 = this;
+
+      return React.createElement(
+        'li',
+        { className: 'list-group-item d-flex justify-content-between align-items-start', id: this.props.todo.id },
+        React.createElement(
+          'div',
+          { className: 'ms-2 me-auto' },
+          React.createElement(
+            'div',
+            { className: 'fw-bold' },
+            this.props.todo.title
+          ),
+          this.props.todo.description
         ),
         React.createElement(
-          'form',
-          { onSubmit: addTodo },
-          React.createElement('input', { type: 'text', name: 'title', className: 'form-control mb-2', placeholder: 'Title' }),
-          React.createElement('textarea', { name: 'description', className: 'form-control mb-2', placeholder: 'Description' }),
-          React.createElement(
-            'button',
-            { className: 'btn btn-sm btn-success' },
-            'Submit'
-          )
+          'button',
+          { className: 'badge bg-primary rounded-pill', onClick: function onClick(e) {
+              return _this5.props.removeTodo(_this5.props.todo.id);
+            } },
+          '\xD7'
         )
-      )
-    )
-  );
+      );
+    }
+  }]);
 
-  ReactDOM.render(template, appRoot);
+  return Todo;
+}(React.Component);
+
+var AddTodo = function (_React$Component4) {
+  _inherits(AddTodo, _React$Component4);
+
+  function AddTodo(props) {
+    _classCallCheck(this, AddTodo);
+
+    var _this6 = _possibleConstructorReturn(this, (AddTodo.__proto__ || Object.getPrototypeOf(AddTodo)).call(this, props));
+
+    _this6.addTodo = _this6.addTodo.bind(_this6);
+    return _this6;
+  }
+
+  _createClass(AddTodo, [{
+    key: 'addTodo',
+    value: function addTodo(e) {
+      e.preventDefault();
+      var title = e.target.elements.title.value;
+      var description = e.target.elements.description.value;
+
+      if (title) {
+        var todo = {
+          id: uuid.v4(),
+          title: title,
+          description: description,
+          date: moment().valueOf()
+        };
+
+        this.props.saveTodo(todo);
+
+        e.target.elements.title.value = '';
+        e.target.elements.description.value = '';
+      }
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'form',
+        { onSubmit: this.addTodo, className: 'mb-2' },
+        React.createElement('input', { type: 'text', name: 'title', className: 'mb-2 form-control', placeholder: 'Title' }),
+        React.createElement('textarea', { name: 'description', className: 'mb-2 form-control', placeholder: 'Description' }),
+        React.createElement(
+          'button',
+          { className: 'btn btn-sm btn-success' },
+          'Add Todo'
+        )
+      );
+    }
+  }]);
+
+  return AddTodo;
+}(React.Component);
+
+var Header = function Header() {
+  return React.createElement(
+    'h1',
+    { className: 'mb-2' },
+    'A Simple React Todos Application'
+  );
 };
 
-render();
+ReactDOM.render(React.createElement(TodoApp, null), appRoot);
--- src/app.js
+++ src/app.js
@@ -1,73 +1,131 @@
 const appRoot = document.getElementById('app')
 
-const getTodos = () => JSON.parse(localStorage.getItem('todos')) || []
-
-const addTodo = (e) => {
-  e.preventDefault()
-  const title = e.target.elements.title.value
-  const description = e.target.elements.description.value
-
-  if (title) {
-    const todos = getTodos()
-    const todo = {
-      id: uuid.v4(), 
-      title: title, 
-      description: description,
-      date: moment().valueOf()
+class TodoApp extends React.Component {
+  constructor(props) {
+    super(props)
+    this.removeTodo = this.removeTodo.bind(this)
+    this.loadTodos = this.loadTodos.bind(this)
+    this.saveTodos = this.saveTodos.bind(this)
+    this.state = {
+      todos: []
     }
-    todos.push(todo)
-    localStorage.setItem('todos', JSON.stringify(todos))
+  }
+  componentDidMount() {
+    this.loadTodos()
+  }
+  removeTodo(id) {
+    this.setState((prevState) => ({
+      todos: prevState.todos.filter(todo => todo.id != id)
+    }))
+    this.saveTodos()
+  }
+  loadTodos() {
+    this.setState(() => ({
+      todos: JSON.parse(localStorage.getItem('todos')) || []  
+    }))
+  }
+  saveTodos(todo) {
+    const todos = this.state.todos
+    if (todo) {
+      todos.push(todo)
+      this.setState(() => ({ todos }))
+    }
     
-    e.target.elements.title.value = ''
-    e.target.elements.description.value = ''
-
-    render()
+    localStorage.setItem('todos', JSON.stringify(this.state.todos))
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="py-2 col-md-6 col-sm-8 col-12 offset-md-3 offset-sm-2">
+            <Header />
+            <AddTodo saveTodo={this.saveTodos} />
+            <Todos todos={this.state.todos} removeTodo={this.removeTodo} />
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
-const renderTodos = () => getTodos().sort((a, b) => a.date >= b.date ? -1 : 1 ).map((todo) => generateDOM(todo))
-
-const generateDOM = (todo) => (
-    <li className="list-group-item d-flex justify-content-between align-items-start" id={todo.id} key={todo.id}>
-      <div className="ms-2 me-auto">
-        <div className="fw-bold">{todo.title}</div>
-        {todo.description}
+class Todos extends React.Component {
+  constructor(props) {
+    super(props)
+    this.renderTodos = this.renderTodos.bind(this)
+  }
+  renderTodos() {
+    return this.props.todos.sort((a, b) => a.date >= b.date ? -1 : 1 ).map((todo) => <Todo key={todo.id} todo={todo} removeTodo={this.props.removeTodo} />)
+  }
+  render() {
+    return (
+      <div>
+        {this.props.todos.length > 0 && <h4>Todos</h4>}
+        {this.props.todos && <ol className="mb-2 list-group list-group-numbered list-group-flush" id="todos">
+          {this.renderTodos()}
+        </ol>}
       </div>
-      <button className="badge bg-primary rounded-pill" onClick={(e) => removeTodo(e, todo.id)}>
-        &times;
-      </button>
-    </li>
-)
-
-const removeTodo = (e, id) => {
-  localStorage.setItem('todos', JSON.stringify(getTodos().filter(todo => todo.id != id)))
-  render()
+    )
+  }
 }
 
-const render = () => {
-
-  const todos = getTodos()
-
-  const template = (
-    <div className="container">
-      <div class="row">
-        <div class="col-md-6 col-sm-8 col-12 offset-md-3 offset-sm-2">
-          <h1>Todo Application</h1>
-          <h4>Todos</h4>
-          {todos && <ol className="list-group mb-2 list-group-numbered list-group-flush" id="todos">
-            {renderTodos()}
-          </ol>}
-          <form onSubmit={addTodo}>
-            <input type="text" name="title" className="form-control mb-2" placeholder="Title"/>
-            <textarea  name="description" className="form-control mb-2" placeholder="Description"></textarea>
-            <button className="btn btn-sm btn-success">Submit</button>
-          </form>
+class Todo extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <li className="list-group-item d-flex justify-content-between align-items-start" id={this.props.todo.id}>
+        <div className="ms-2 me-auto">
+          <div className="fw-bold">{this.props.todo.title}</div>
+          {this.props.todo.description}
         </div>
-      </div>
-    </div>
-  )
-  
-  ReactDOM.render(template, appRoot)
+        <button className="badge bg-primary rounded-pill" onClick={(e) => this.props.removeTodo(this.props.todo.id)}>
+          &times;
+        </button>
+      </li>
+    )
+  }
 }
 
-render()
+class AddTodo extends React.Component {
+  constructor(props) {
+    super(props)
+    this.addTodo = this.addTodo.bind(this)
+  }
+  addTodo(e) {
+    e.preventDefault()
+    const title = e.target.elements.title.value
+    const description = e.target.elements.description.value
+
+    if (title) {
+      const todo = {
+        id: uuid.v4(), 
+        title: title, 
+        description: description,
+        date: moment().valueOf()
+      }
+
+      this.props.saveTodo(todo)
+      
+      e.target.elements.title.value = ''
+      e.target.elements.description.value = ''
+    }
+  }
+  render() {
+    return (
+      <form onSubmit={this.addTodo} className="mb-2">
+        <input type="text" name="title" className="mb-2 form-control" placeholder="Title"/>
+        <textarea  name="description" className="mb-2 form-control" placeholder="Description"></textarea>
+        <button className="btn btn-sm btn-success">Add Todo</button>
+      </form>
+    )
+  }
+}
+
+const Header = () => {
+  return (
+    <h1 className="mb-2">A Simple React Todos Application</h1>
+  )
+}
+
+ReactDOM.render(<TodoApp />, appRoot)
